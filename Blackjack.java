a14f6650b2373b4793bf38f7ac16e85c76eb1ae4
jacobprimehd <35090895+jacobprimehd@users.noreply.github.com>
2018-01-04 08:33:41 -0800

    Update Blackjack.java

--- Blackjack.java
+++ Blackjack.java
@@ -1,1 +1,12 @@
-public class Blackjack
+import java.util.*;
+
+public class Blackjack {
+   public static void main(String[] args){
+      Scanner console = new Scanner(System.in);
+      System.out.println("Welcome to Blackjack!");
+      System.out.print("Please enter the name of player 1: ");
+      String player1 = console.next();
+      System.out.print("\nPlease enter the name of player 2: ");
+      String player2 = console.next();
+   }
+}
