a16283c6ae7b605f7a57efc1f2280b138146bb12
jacobprimehd <35090895+jacobprimehd@users.noreply.github.com>
2018-01-04 08:55:05 -0800

    Update Blackjack.java

--- Blackjack.java
+++ Blackjack.java
@@ -4,13 +4,16 @@ public class Blackjack {
    public static void main(String[] args){
       
       Scanner console = new Scanner(System.in);
-      Player p1 = new Player;
-      Player p2 = new Player;
+      Player p1 = new Player; //player 1 
+      Player p2 = new Player;// Player 2
       
       System.out.println("Welcome to Blackjack!");
       System.out.print("Please enter the name of player 1: ");
       p1.name = console.next();
       System.out.print("\nPlease enter the name of player 2: ");
       p2.name = console.next();
+      System.out.println("=========================");
+      System.out.println(p1 + " shows: ");
+      System.out.println(p2 + " shows: ");
    }
 }
