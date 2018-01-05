c3807a52797b658ba46953cc487f3ccdbba4f9a1
yaqwolfe <35109166+yaqwolfe@users.noreply.github.com>
2018-01-05 08:54:45 -0800

    Add files via upload

--- action.java
+++ action.java
@@ -1,11 +1,19 @@
 private string action{
+   private string output;
 
    //public string stand(){
    //system.out.println("stand");
    //}
-   public string hit(){
-   get.random card;
    
-   return card
+   public string hit(){
+      string suit = deck.setRandomSuit();
+      string card = deck.setRandomCard();
+   }
+   
+   public string getOutput{
+      return getOutput;
+   }
+   
+   this.output = system.out.print(suit + " " + card);
    }
 }
