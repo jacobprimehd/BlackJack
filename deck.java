76e50f8d21bca432a99c6683d0147c947cbb0b33
pottdan20 <35109135+pottdan20@users.noreply.github.com>
2018-01-05 08:57:13 -0800

    Add files via upload

--- deck.java
+++ deck.java
@@ -1,25 +1,109 @@
+import java.util.*;
 public class deck{
-   private String suits;
-   private int[] drawnCards;
-   int[] spades = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-   int[] hearts = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-   int[] diamonds = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-   int[] clubs = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-   int suitNum = (int)Math.random()*4+1;
-   String suit = "";
+   private String suit;
+   private int cardValue;
+   private String cardName;
+   ArrayList<Integer> spades = new ArrayList<>();
+   ArrayList<Integer> hearts = new ArrayList<>();
+      ArrayList<Integer> diamonds = new ArrayList<>();
+   ArrayList<Integer> clubs = new ArrayList<>();
+   int cardNum = 0;
+  
+   
+   int suitNum = (int)Math.random()*4+1;;
+   public void shuffle(){
+      for(int i = 1; i <= 13; i++){
+         spades.add(i);
+      }
+       for(int i = 1; i <= 13; i++){
+         hearts.add(i);
+      }
+       for(int i = 1; i <= 13; i++){
+         clubs.add(i);
+      }
+       for(int i = 1; i <= 13; i++){
+         diamonds.add(i);
+      }
+    
+   }
    public void setRandomSuit(){   
      if(suitNum == 1){
-      suit = Hearts;
+      this.suit = "Hearts";
      } else if(suitNum == 2){
-      suit = Spades;
-     } elseif(suitNum == 3){
-      suit = Clubs;
-     } elseif(suitNum == 4){
-      suit = Diamonds;
+      this.suit = "Spades";
+     } else if(suitNum == 3){
+      this.suit = "Clubs";
+     } else if(suitNum == 4){
+      this.suit = "Diamonds";
      }
    }
    
-   public String getRandomSuit(suit){
+   public String getRandomSuit(){
     return suit;
+   }
+   public void setRandomCard(){
+      // int cardNum = 0;
+      if(suit.equals("Hearts")){
+          cardNum = ((int)Math.random())*(hearts.size())+1;
+          hearts.remove(cardNum-1);
+      }else if(suit.equals("Spades")){
+          cardNum = ((int)Math.random())*(hearts.size())+1;
+          spades.remove(cardNum-1);
+      }else if(suit.equals("Clubs")){
+          cardNum = ((int)Math.random())*(hearts.size())+1;
+          clubs.remove(cardNum-1);
+      }else if(suit.equals("Diamonds")){
+          cardNum = ((int)Math.random())*(hearts.size())+1;
+          diamonds.remove(cardNum-1);
+      }
+   }
+
+   public void setCardValueAndName(){
+      if(cardNum == 1){
+         this.cardValue = 1;
+         this.cardName = "ace";
+      }else if(cardNum == 2){
+         this.cardValue = 2;
+         this.cardName = "two";
+      }else if(cardNum == 3){
+         this.cardValue = 3;
+         this.cardName = "three";
+      }else if(cardNum == 4){
+         this.cardValue = 4;
+         this.cardName = "four";
+      }else if(cardNum == 5){
+         this.cardValue = 5;
+         this.cardName = "five";
+      }else if(cardNum == 6){
+         this.cardValue = 6;
+         this.cardName = "six";
+      }else if(cardNum == 7){
+         this.cardValue = 7;
+         this.cardName = "seven";
+      }else if(cardNum == 8){
+         this.cardValue = 8;
+         this.cardName = "eight";
+      }else if(cardNum == 9){
+         this.cardValue = 9;
+         this.cardName = "nine";
+      }else if(cardNum == 10){
+         this.cardValue = 10;
+         this.cardName = "ten";
+      }else if(cardNum == 11){
+         this.cardValue = 10;
+         this.cardName = "Jack";
+      }else if(cardNum == 12){
+         this.cardValue = 10;
+         this.cardName = "Queen";
+      }else if(cardNum == 13){
+         this.cardValue = 10;
+         this.cardName = "King";
+      }
+   }
+   public int getCardValue(){
+      return this.cardValue;
+   }
+   public String getCardName(){
+      return this.cardName;
    }
 }
